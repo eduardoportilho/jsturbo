89b4bd43a3610edc2431bb57728dbd763f10cc3d
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-01-28 20:10:16 +0100

    feat(str): String padding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-function foo () {
-  return 'bar'
-}
+
+/**
+ * Apeend the given char into the left of the string so change its length to the provided value
+ * ex: pad('10', 4, '-'); //> '--10'
+ */
+function pad (string, newLength, padChar) {
+  padChar = padChar || '0'
+  let paddedString = string + ''
+  return paddedString.length >= newLength ? paddedString : new Array(newLength - paddedString.length + 1).join(padChar) + paddedString
+};
 
 const mainExport = {
-  foo: foo
+  str: {
+    pad: pad
+  }
 }
 
 export default mainExport
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,25 @@
 import {expect} from 'chai'
 import jsturbo from '../src/index'
 
-describe('index', function() {
-  describe('foo', function() {
-    it('should return bar', function() {
-      expect(jsturbo.foo()).to.equal('bar')
+describe('str', function() {
+  let str = jsturbo.str
+
+  describe('pad', function() {
+
+    it('should not change string with same length', function() {
+      expect(str.pad('test', 4)).to.equal('test')
+      expect(str.pad('test', 3)).to.equal('test')
     })
+
+    it('should pad with zeros', function() {
+      expect(str.pad('test', 5)).to.equal('0test')
+      expect(str.pad('test', 7)).to.equal('000test')
+    })
+    
+    it('should pad with custom char', function() {
+      expect(str.pad('test', 5, 'z')).to.equal('ztest')
+      expect(str.pad('test', 7, 'z')).to.equal('zzztest')
+    })
+
   })
 })
