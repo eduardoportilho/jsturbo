1d4a35305a3a052d4116a5e61ec5bd88fa094915
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-01-29 11:31:33 +0100

    feat(str): String isNumber test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 
 /**
- * Apeend the given char into the left of the string so change its length to the provided value
+ * Apeend the given char into the left of the string so change its length to the provided value.
  * ex: pad('10', 4, '-'); //> '--10'
+ * @param string String to be padded
+ * @param newLength Length of the padded string
+ * @param padChar Char used to pad the string, default = '0'
+ * @return Padded string
  */
 function pad (string, newLength, padChar) {
   padChar = padChar || '0'
@@ -9,9 +13,28 @@ function pad (string, newLength, padChar) {
   return paddedString.length >= newLength ? paddedString : new Array(newLength - paddedString.length + 1).join(padChar) + paddedString
 };
 
+/**
+ * Test if the string represent a valid number.
+ * @param string String to be tested
+ * @param decimalSeparator Character used as decimal separator, default = '.'
+ * @return true|false
+ */
+function isNumber (string, decimalSeparator) {
+  var str = string
+  if (decimalSeparator) {
+    // special case: if the decimal separator is not '.', any dot make the string not a number
+    if (decimalSeparator !== '.' && str.indexOf('.') >= 0) {
+      return false
+    }
+    str = string.replace(decimalSeparator, '.')
+  }
+  return !isNaN(Number(str))
+};
+
 const mainExport = {
   str: {
-    pad: pad
+    pad: pad,
+    isNumber: isNumber
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,10 @@ import jsturbo from '../src/index'
 describe('str', function() {
   let str = jsturbo.str
 
+  /**
+   * str.pad
+   */
   describe('pad', function() {
-
     it('should not change string with same length', function() {
       expect(str.pad('test', 4)).to.equal('test')
       expect(str.pad('test', 3)).to.equal('test')
@@ -15,11 +17,34 @@ describe('str', function() {
       expect(str.pad('test', 5)).to.equal('0test')
       expect(str.pad('test', 7)).to.equal('000test')
     })
-    
+
     it('should pad with custom char', function() {
       expect(str.pad('test', 5, 'z')).to.equal('ztest')
       expect(str.pad('test', 7, 'z')).to.equal('zzztest')
     })
+  });
 
+  /**
+   * str.isNumber
+   */
+  describe("isNumber", function() {
+    it("should indentify valid numbers", function() {
+      expect(str.isNumber('10')).to.be.true;
+      expect(str.isNumber('10.9')).to.be.true;
+    });
+
+    it("should indentify invalid numbers", function() {
+      expect(str.isNumber('10x')).to.be.false;
+      expect(str.isNumber('1$')).to.be.false;
+      expect(str.isNumber('10.9.2')).to.be.false;
+    });
+
+    it("should indentify valid numbers with custom separators", function() {
+      expect(str.isNumber('10,123', ',')).to.be.true;
+    });
+
+    it("should indentify invalid numbers with custom separators", function() {
+      expect(str.isNumber('1.', ',')).to.be.false;
+    });
   })
 })
