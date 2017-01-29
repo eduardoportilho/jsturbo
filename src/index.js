0daedacfc9ff8b3b01bb021e77833371a2482742
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-01-29 11:55:48 +0100

    feat(str): String to number conversion

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 /**
  * Apeend the given char into the left of the string so change its length to the provided value.
  * ex: pad('10', 4, '-'); //> '--10'
- * @param string String to be padded
- * @param newLength Length of the padded string
- * @param padChar Char used to pad the string, default = '0'
- * @return Padded string
+ * @param {string} string String to be padded
+ * @param {number} newLength Length of the padded string
+ * @param {string} padChar Char used to pad the string, default = '0'
+ * @return {string} Padded string
  */
 function pad (string, newLength, padChar) {
   padChar = padChar || '0'
@@ -15,9 +15,9 @@ function pad (string, newLength, padChar) {
 
 /**
  * Test if the string represent a valid number.
- * @param string String to be tested
- * @param decimalSeparator Character used as decimal separator, default = '.'
- * @return true|false
+ * @param {string} string String to be tested
+ * @param {string} decimalSeparator Character used as decimal separator, default = '.'
+ * @return {boolean} String is a number?
  */
 function isNumber (string, decimalSeparator) {
   var str = string
@@ -31,10 +31,33 @@ function isNumber (string, decimalSeparator) {
   return !isNaN(Number(str))
 };
 
+/**
+ * Convert a string to a number on a pretty lenient way.
+ * @param  {string} String to be converted
+ * @return {number|NaN} Number or NaN if not possible
+ */
+function toNumber (string) {
+  if (string === null || string === undefined) return NaN
+  var convertedString = string
+    .replace(/[^\d.,]/g, '')  // remove everything except numbers, '.' and ','
+    .replace(/,/g, '.')       // replaces ',' for '.'
+  var idxUltimoPonto = convertedString.lastIndexOf('.')
+  if (idxUltimoPonto > 0) {
+    var parteInteira = convertedString.substring(0, idxUltimoPonto).replace(/\./g, '')
+    convertedString = parteInteira + '.' + convertedString.substring(idxUltimoPonto + 1)
+  }
+  if (convertedString.trim().length === 0) return NaN
+
+  var signal = string.indexOf('-') === 0 ? -1 : 1
+  // make sure that the return is a number and fix the signal
+  return convertedString * signal
+};
+
 const mainExport = {
   str: {
     pad: pad,
-    isNumber: isNumber
+    isNumber: isNumber,
+    toNumber: toNumber
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -8,43 +8,96 @@ describe('str', function() {
    * str.pad
    */
   describe('pad', function() {
-    it('should not change string with same length', function() {
-      expect(str.pad('test', 4)).to.equal('test')
-      expect(str.pad('test', 3)).to.equal('test')
-    })
+  it('should not change string with same length', function() {
+    expect(str.pad('test', 4)).to.equal('test')
+    expect(str.pad('test', 3)).to.equal('test')
+  })
 
-    it('should pad with zeros', function() {
-      expect(str.pad('test', 5)).to.equal('0test')
-      expect(str.pad('test', 7)).to.equal('000test')
-    })
+  it('should pad with zeros', function() {
+    expect(str.pad('test', 5)).to.equal('0test')
+    expect(str.pad('test', 7)).to.equal('000test')
+  })
 
-    it('should pad with custom char', function() {
-      expect(str.pad('test', 5, 'z')).to.equal('ztest')
-      expect(str.pad('test', 7, 'z')).to.equal('zzztest')
-    })
+  it('should pad with custom char', function() {
+    expect(str.pad('test', 5, 'z')).to.equal('ztest')
+    expect(str.pad('test', 7, 'z')).to.equal('zzztest')
+  })
   });
 
   /**
    * str.isNumber
    */
   describe("isNumber", function() {
-    it("should indentify valid numbers", function() {
-      expect(str.isNumber('10')).to.be.true;
-      expect(str.isNumber('10.9')).to.be.true;
-    });
+  it("should indentify valid numbers", function() {
+    expect(str.isNumber('10')).to.be.true;
+    expect(str.isNumber('10.9')).to.be.true;
+  });
 
-    it("should indentify invalid numbers", function() {
-      expect(str.isNumber('10x')).to.be.false;
-      expect(str.isNumber('1$')).to.be.false;
-      expect(str.isNumber('10.9.2')).to.be.false;
-    });
+  it("should indentify invalid numbers", function() {
+    expect(str.isNumber('10x')).to.be.false;
+    expect(str.isNumber('1$')).to.be.false;
+    expect(str.isNumber('10.9.2')).to.be.false;
+  });
 
-    it("should indentify valid numbers with custom separators", function() {
-      expect(str.isNumber('10,123', ',')).to.be.true;
-    });
+  it("should indentify valid numbers with custom separators", function() {
+    expect(str.isNumber('10,123', ',')).to.be.true;
+  });
 
-    it("should indentify invalid numbers with custom separators", function() {
-      expect(str.isNumber('1.', ',')).to.be.false;
-    });
+  it("should indentify invalid numbers with custom separators", function() {
+    expect(str.isNumber('1.', ',')).to.be.false;
+  });
   })
+  /**
+   * str.toNumber
+   */
+  describe("toNumber", function() {
+    it("should be able to recognize different decimal and thousand separators", function() {
+      var num = str.toNumber('1.234,56')
+      expect(num).to.equal(1234.56);
+
+      var num2 = str.toNumber('1,234.56')
+      expect(num2).to.equal(1234.56);
+    });
+
+    it("should be able to ignore thousand separators", function() {
+      var num = str.toNumber('1.234.567,89')
+      expect(num).to.equal(1234567.89);
+    });
+
+    it("should be able to ignore wrong separators", function() {
+      var num = str.toNumber('1.234.567.89')
+      expect(num).to.equal(1234567.89);
+    });
+
+    it("should be able to ignore currency signs", function() {
+      var num = str.toNumber('R$ 1.234,56')
+      expect(num).to.equal(1234.56);
+
+      var num2 = str.toNumber('USD 1,234.56')
+      expect(num2).to.equal(1234.56);
+    });
+
+    it("should be able to ignore other characters", function() {
+      var num = str.toNumber('"""1.234,56"""')
+      expect(num).to.equal(1234.56);
+
+      var num2 = str.toNumber('#@W1,234.56CF&*Y')
+      expect(num2).to.equal(1234.56);
+    });
+
+    it("should return NaN on undefined, null or not a number", function() {
+      expect(str.toNumber('')).to.be.NaN;
+      expect(str.toNumber('notanumber')).to.be.NaN;
+      expect(str.toNumber(null)).to.be.NaN;
+      expect(str.toNumber(undefined)).to.be.NaN;
+    });
+
+    it("should recognize negative numbers", function() {
+      var num = str.toNumber('-1.234,56')
+      expect(num).to.equal(-1234.56);
+
+      var num = str.toNumber('+1.234,56')
+      expect(num).to.equal(1234.56);
+    });
+  });
 })
